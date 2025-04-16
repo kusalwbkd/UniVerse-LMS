5b6cdbdb6b4a06ad9b81f2324c18fbd0aa694bf7
kuslwbkd <kusalwbkddhananjaya@gmail.com>
2025-04-16 10:13:48 +0530

    small fix

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,9 +2,11 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { createNewUser, GenerateNotes, helloWorld } from "../../../inngest/functions";
 
+export const runtime='edge'
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
+  streaming:'allow',
   functions: [
     createNewUser,
     GenerateNotes
